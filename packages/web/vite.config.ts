53f6f61b7b26dc8a72aa5324853082c670e04f59
lzy <2397925443@qq.com>
2024-05-26 17:51:59 +0800

    feat: 初始化项目

---
+++ packages/web/vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import taiwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: true
+  },
+  css: {
+    postcss: {
+      plugins: [taiwindcss(), autoprefixer()]
+    }
+  }
+})
