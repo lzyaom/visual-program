1b0964eef58d0c8e65d4ee22f61da5a2ba9cb673
lzy <2397925443@qq.com>
2024-06-04 02:06:23 +0800

    feat: 添加编程列表页面布局

--- packages/web/src/views/program/index.vue
+++ packages/web/src/views/program/index.vue
@@ -1,6 +1,117 @@
-<script setup lang="ts"></script>
-<template>
-  <div class="page-program">编程文件列表</div>
-</template>
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
 
-<style scoped></style>
+const invoices = [
+  {
+    invoice: 'INV001',
+    paymentStatus: 'Paid',
+    totalAmount: '$250.00',
+    paymentMethod: 'Credit Card'
+  },
+  {
+    invoice: 'INV002',
+    paymentStatus: 'Pending',
+    totalAmount: '$150.00',
+    paymentMethod: 'PayPal'
+  },
+  {
+    invoice: 'INV003',
+    paymentStatus: 'Unpaid',
+    totalAmount: '$350.00',
+    paymentMethod: 'Bank Transfer'
+  },
+  {
+    invoice: 'INV004',
+    paymentStatus: 'Paid',
+    totalAmount: '$450.00',
+    paymentMethod: 'Credit Card'
+  },
+  {
+    invoice: 'INV005',
+    paymentStatus: 'Paid',
+    totalAmount: '$550.00',
+    paymentMethod: 'PayPal'
+  },
+  {
+    invoice: 'INV006',
+    paymentStatus: 'Pending',
+    totalAmount: '$200.00',
+    paymentMethod: 'Bank Transfer'
+  },
+  {
+    invoice: 'INV007',
+    paymentStatus: 'Unpaid',
+    totalAmount: '$300.00',
+    paymentMethod: 'Credit Card'
+  }
+]
+
+const statusOptions = [
+  { label: 'Apple', value: 'apple' },
+  { label: 'Banana', value: 'banana' },
+  { label: 'Blueberry', value: 'blueberry' },
+  { label: 'Grapes', value: 'grapes' },
+  { label: 'Pineapple', value: 'pineapple' }
+]
+</script>
+<template>
+  <div class="page-program px-4">
+    <div class="filter mb-4">
+      <Select>
+        <SelectTrigger class="w-[180px]">
+          <SelectValue placeholder="Select a status" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            <SelectItem v-for="item in statusOptions" :key="item.label" :value="item.value">{{
+              item.label
+            }}</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+    <div class="border rounded-md">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead></TableHead>
+            <TableHead>Task</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Descript</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow v-for="invoice in invoices" :key="invoice.invoice">
+            <TableCell></TableCell>
+            <TableCell>
+              {{ invoice.invoice }}
+            </TableCell>
+            <TableCell>{{ invoice.paymentStatus }}</TableCell>
+            <TableCell>{{ invoice.paymentMethod }}</TableCell>
+            <TableCell>
+              {{ invoice.totalAmount }}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  </div>
+</template>
