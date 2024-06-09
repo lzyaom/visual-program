d7d88694e30ebc5ea0edd08e9dd3f7474baa6985
lzy <2397925443@qq.com>
2024-06-10 02:44:11 +0800

    fix: 修改 Table 组件

---
+++ packages/web/src/components/ui/table/index.ts
@@ -0,0 +1,2 @@
+export { TableRoot as Table } from './Table'
+export * from './type'
---
+++ packages/web/src/components/ui/table/type.ts
@@ -0,0 +1,11 @@
+export interface Columns {
+  prop: string
+  label?: string
+  isSlot?: boolean
+  attrs?: ColumnsAttrs
+  icon?: String
+}
+
+export interface ColumnsAttrs {
+  class: string
+}
