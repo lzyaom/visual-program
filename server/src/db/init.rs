7a0aaf45c045ddf9d172353114b326c6e148d91f
lzy <2397925443@qq.com>
2024-06-11 08:10:03 +0800

    feat: 添加 mongodb

---
+++ server/src/db/init.rs
@@ -0,0 +1,14 @@
+use std::env;
+use mongodb::{Client, options::ClientOptions};
+
+pub async fn init_mongo_client() -> mongodb::error::Result<Client> {
+  let url = env::var("DATABASE_URL").expect("database url must set");
+
+  let mut client_options = ClientOptions::parse(&url).await?;
+
+  client_options.app_name = Some("Program".to_string());
+
+  let client = Client::with_options(client_options)?;
+
+  Ok(client)
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,17 @@
-use axum::Router;
-use server::router::init_router;
+use std::{sync::Arc, env};
+
+use axum::{Extension, Router};
+use server::{db, router::init_router};
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
+    let client = db::init_mongo_client().await.unwrap();
+    let db_name = env::var("DATABASE_NAME").expect("database name must set");
+    let progrom_db = client.database(&db_name);
+    let share_db = Arc::new(progrom_db);
     let api_routes = init_router();
-    let app = Router::new().nest("/api", api_routes);
+    let app = Router::new().nest("/api", api_routes).layer(Extension(share_db));
     // run our app with hyper
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
