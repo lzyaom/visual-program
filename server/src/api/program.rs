ddd2d6b51d90d6d16a94afef78377358bb2b9358
lzy <2397925443@qq.com>
2024-06-13 00:54:03 +0800

    fix: 修改 program 数据库中 collection 名称

--- server/src/api/program.rs
+++ server/src/api/program.rs
@@ -6,9 +6,9 @@ use serde_json::json;
 
 /// 获取程序列表
 pub async fn get_program_list(db: Extension<ShareDB>) -> impl IntoResponse {
-    let collection: Collection<Program> = db.collection("programes");
+    let collection: Collection<Program> = db.collection("program");
 
-    let find_options = FindOptions::builder().sort(doc! {"createTime": -1}).build();
+    let find_options = FindOptions::builder().sort(doc! {"create_time": -1}).build();
 
     let cursor = collection.find(None, find_options).await.unwrap();
 
@@ -19,7 +19,7 @@ pub async fn get_program_list(db: Extension<ShareDB>) -> impl IntoResponse {
 
 /// 通过 `id` 获取程序 [`Program`]
 pub async fn get_program(db: Extension<ShareDB>, Path(id): Path<String>) -> impl IntoResponse {
-    let collection: Collection<Program> = db.collection("programes");
+    let collection: Collection<Program> = db.collection("program");
 
     let id = ObjectId::parse_str(&id).unwrap();
 
@@ -35,7 +35,7 @@ pub async fn get_program(db: Extension<ShareDB>, Path(id): Path<String>) -> impl
 pub async fn get_program_schema(db: Extension<ShareDB>, Path(id): Path<String>) -> impl IntoResponse {
     let id = ObjectId::parse_str(&id).unwrap();
 
-    let collection: Collection<Program> = db.collection("programes");
+    let collection: Collection<Program> = db.collection("program");
 
     let program = collection.find_one(doc! {"_id": id}, None).await.unwrap();
 
@@ -51,7 +51,7 @@ pub async fn create_program_schema(db: Extension<ShareDB>, Json(data): Json<Prog
 
     data.id = Some(ObjectId::new());
 
-    let collection: Collection<Program> = db.collection("progromes");
+    let collection: Collection<Program> = db.collection("program");
     let result = collection.insert_one(data, None).await;
 
     match result {
@@ -64,7 +64,7 @@ pub async fn create_program_schema(db: Extension<ShareDB>, Json(data): Json<Prog
 pub async fn update_program_schema(db: Extension<ShareDB>, Path(id): Path<String>, Json(data): Json<Program>) -> impl IntoResponse {
     let id = ObjectId::parse_str(&id).unwrap();
 
-    let collection: Collection<Program> = db.collection("programes");
+    let collection: Collection<Program> = db.collection("program");
 
     let result = collection.find_one_and_replace(doc! {"_id": id}, data, None).await.unwrap();
 
@@ -80,12 +80,15 @@ pub async fn create_program(db: Extension<ShareDB>, Json(data): Json<Program>) -
 
     data.id = Some(ObjectId::new());
 
-    let collection: Collection<Program> = db.collection("progromes");
+    let collection: Collection<Program> = db.collection("program");
 
     let result = collection.insert_one(data, None).await;
 
     match result {
-        Ok(_) => (StatusCode::OK).into_response(),
+        Ok(value) => {
+            println!("{:#?}", value);
+            (StatusCode::OK).into_response()
+        },
         Err(_) => (StatusCode::INTERNAL_SERVER_ERROR).into_response()
     }
 }
@@ -94,7 +97,7 @@ pub async fn create_program(db: Extension<ShareDB>, Json(data): Json<Program>) -
 pub async fn update_program(db: Extension<ShareDB>, Path(id): Path<String>, Json(data): Json<Program>) -> impl IntoResponse {
     let id = ObjectId::parse_str(&id).unwrap();
 
-    let collection: Collection<Program> = db.collection("programes");
+    let collection: Collection<Program> = db.collection("program");
 
     let result = collection.find_one_and_replace(doc! {"_id": id}, data, None).await.unwrap();
 
@@ -108,7 +111,7 @@ pub async fn update_program(db: Extension<ShareDB>, Path(id): Path<String>, Json
 pub async fn detele_program(db: Extension<ShareDB>, Path(id): Path<String>) -> impl IntoResponse {
     let id = ObjectId::parse_str(&id).map_err(|_| StatusCode::BAD_REQUEST).unwrap();
 
-    let collection: Collection<Program> = db.collection("programes");
+    let collection: Collection<Program> = db.collection("program");
 
     let result = collection.delete_one(doc! {"_id": id}, None).await.unwrap();
 
