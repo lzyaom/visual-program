1c980dc4f6a23d7f4c11e95ee8da0a6b93747548
lzy <2397925443@qq.com>
2024-06-21 16:27:08 +0800

    fix(server): 修改 program 中的 status 为多状态

--- server/src/models/program.rs
+++ server/src/models/program.rs
@@ -14,7 +14,8 @@ pub struct Program {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
     pub title: String,
-    pub status: ProgramStatus,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub status: Option<Vec<ProgramStatus>>,
     pub descript: String,
     pub create_time: String,
 }
@@ -24,7 +25,7 @@ pub struct Program {
 pub struct ProgramQuery {
     pub title: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub status: ProgramStatus,
+    pub status: Option<Vec<ProgramStatus>>,
     pub page: i64,
     pub size: i64,
 }
