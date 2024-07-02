1f0e608e257f262670014e938c170c06f0eb7092
lzyaom <2397925443@qq.com>
2024-07-03 00:26:50 +0800

    fix(server): 修改执行生成 python 文件的方式

--- server/src/compile/run/python.rs
+++ server/src/compile/run/python.rs
@@ -1,43 +1,13 @@
 use pyo3::prelude::*;
 use pyo3::types::PyModule;
 
-pub fn run(_source_code: &str) -> PyResult<()> {
+pub fn run(source_code: &str) -> PyResult<()> {
+    // 初始化 python 解析器
     Python::with_gil(|py| {
-        let script = r#"
-import trace
-import sys
+        let module = PyModule::from_code_bound(py, &source_code, "runtime.py", "runtime")?;
 
-def sample_function():
-    print("Hello from Python!")
-
-def another_function():
-    print("Another function in Python.")
-
-class ReportTrace(trace.Trace):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def localtrace_trace(self, frame, event, arg):
-        if event == 'call':
-            print(f'Calling {frame.f_code.co_name} at line {frame.f_lineno}')
-        elif event == 'line':
-            print(f'Executing line {frame.f_lineno} in {frame.f_code.co_name}')
-        return self.localtrace_trace
-
-# Create a Trace object, telling it what to ignore, and whether to
-# do tracing or line-counting or both.
-tracer = ReportTrace(
-    ignoredirs=[sys.prefix, sys.exec_prefix],
-    trace=True,
-    count=False,
-)
-
-# Execute the script under the control of the Trace object.
-tracer.run('sample_function()')
-tracer.run('another_function()')
-"#;
-        PyModule::from_code_bound(py, &script, "runtime", "")?;
-
+        // 调用函数
+        module.call_method0("main")?;
         Ok(())
     })
 }
