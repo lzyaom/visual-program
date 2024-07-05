fba8784c9264201ecf5d4d082539ed1300703b56
lzyaom <2397925443@qq.com>
2024-07-05 10:17:10 +0800

    feat(web): 添加程序编辑页面布局

--- packages/web/src/views/program/page/Editor.vue
+++ packages/web/src/views/program/page/Editor.vue
@@ -1,9 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui'
+</script>
 
 <template>
-  <div>
-    <h1>Editor</h1>
+  <div class="editor-container">
+    <div class="tool-bar">
+      <div class="back"></div>
+      <h2 class="title"></h2>
+      <div class="tool-bar">
+        <button>New</button>
+        <button>Open</button>
+        <button>Save</button>
+        <button>Forward</button>
+        <button>Back</button>
+        <button>Import</button>
+        <button>Export</button>
+        <button>Help</button>
+        <button>Settings</button>
+      </div>
+      <div class="operate">
+        <button>Run</button>
+        <button>Stop</button>
+        <button>Debug</button>
+      </div>
+    </div>
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel>
+        <div class="command-panel"></div>
+      </ResizablePanel>
+      <ResizableHandle with-handle />
+      <ResizablePanel>
+        <div class="command-tree"></div>
+      </ResizablePanel>
+      <ResizableHandle with-handle />
+      <ResizablePanel>
+        <div class="property-panel"></div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
   </div>
 </template>
-
-<style scoped></style>
